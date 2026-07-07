5a383e0714d2c55fb80660dfe6a1f8e4240d3aa8
argusofwater <argusofwater@gmail.com>
2026-07-07 16:32:40 -0600

    Centralize Commander setting registration

--- src/module/commander/hooks.js
+++ src/module/commander/hooks.js
@@ -1,11 +1,9 @@
-import { registerCommanderSettings } from "./settings.js";
 import { registerCommanderDataModels } from "./data/register.js";
 import { createCommanderController } from "./controller.js";
 
 export const CommanderHooks = {
   listen() {
     Hooks.once("init", () => {
-      registerCommanderSettings();
       const enabled = game.settings.get("ptu", "commanderEnabled");
       const confirmed = game.settings.get("ptu", "commanderMigrationConfirmed");
       if (enabled && confirmed) registerCommanderDataModels();
