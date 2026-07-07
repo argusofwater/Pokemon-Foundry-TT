6feba457c31004d8db5acb2e1bc2f4cb182d631f
argusofwater <argusofwater@gmail.com>
2026-07-07 14:43:26 -0600

    Guard legacy actor preparation for Commander models

--- src/module/commander/data/register.js
+++ src/module/commander/data/register.js
@@ -7,8 +7,35 @@ import {
   CommanderCapabilityData
 } from "./item-models.js";
 import { CommanderSpeciesData } from "./species-model.js";
+import { PTUActor } from "../../actor/base.js";
+import { PTUTrainerActor } from "../../actor/character/document.js";
+import { PTUPokemonActor } from "../../actor/pokemon/document.js";
+
+let preparationGuardsInstalled = false;
+
+function installCommanderPreparationGuards() {
+  if (preparationGuardsInstalled) return;
+  preparationGuardsInstalled = true;
+
+  for (const ActorClass of [PTUTrainerActor, PTUPokemonActor]) {
+    const legacyPrepareBaseData = ActorClass.prototype.prepareBaseData;
+    const legacyPrepareDerivedData = ActorClass.prototype.prepareDerivedData;
+
+    ActorClass.prototype.prepareBaseData = function() {
+      if (this.system?.schema) return PTUActor.prototype.prepareBaseData.call(this);
+      return legacyPrepareBaseData.call(this);
+    };
+
+    ActorClass.prototype.prepareDerivedData = function() {
+      if (this.system?.schema) return PTUActor.prototype.prepareDerivedData.call(this);
+      return legacyPrepareDerivedData.call(this);
+    };
+  }
+}
 
 export function registerCommanderDataModels() {
+  installCommanderPreparationGuards();
+
   CONFIG.Actor.dataModels.character = CommanderTrainerData;
   CONFIG.Actor.dataModels.pokemon = CommanderPokemonData;
 
