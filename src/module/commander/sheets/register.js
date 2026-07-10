08adfa2b1c2f5a444a3bc77d1a9bab0734f08bad
argusofwater <argusofwater@gmail.com>
2026-07-10 17:59:10 -0600

    Use Foundry V14 API for Commander sheets

--- src/module/commander/sheets/register.js
+++ src/module/commander/sheets/register.js
@@ -2,13 +2,15 @@ import { CommanderTrainerSheet } from "./trainer-sheet.js";
 import { CommanderPokemonSheet } from "./pokemon-sheet.js";
 
 export function registerCommanderSheets() {
-  foundry.documents.collections.Actors.registerSheet("ptu-commander", CommanderTrainerSheet, {
+  const SheetConfig = foundry.applications.apps.DocumentSheetConfig;
+
+  SheetConfig.registerSheet(CONFIG.Actor.documentClass, "ptu-commander", CommanderTrainerSheet, {
     types: ["character"],
     label: "Commander Trainer Sheet",
     makeDefault: true
   });
 
-  foundry.documents.collections.Actors.registerSheet("ptu-commander", CommanderPokemonSheet, {
+  SheetConfig.registerSheet(CONFIG.Actor.documentClass, "ptu-commander", CommanderPokemonSheet, {
     types: ["pokemon"],
     label: "Commander Creature Sheet",
     makeDefault: true
